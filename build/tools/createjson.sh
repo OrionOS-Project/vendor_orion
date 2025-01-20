a6d4e2cec01f1ef368037ddb232cb2b17f2aa77c
Wrdn28 <ifloraxd@gmail.com>
2025-01-21 03:50:20 +0700

    tools: Refactor json for orion ota updater

--- build/tools/createjson.sh
+++ build/tools/createjson.sh
@@ -16,7 +16,7 @@
 #
 
 #$1=TARGET_DEVICE, $2=PRODUCT_OUT, $3=FILE_NAME, $4=WITH_GMS
-existingOTAjson=./orion/OTA/builds/$1.json
+existingOTAjson=./orion/orion_ota/builds/$1.json
 output=$2/$1.json
 
 #cleanup old file
@@ -27,11 +27,11 @@ fi
 if [ "$4" = "true" ]; then
     echo "Generating JSON for OTA..."
     variant="A15"
-    existingOTAjson=./orion/OTA/builds/$1.json
+    existingOTAjson=./orion/orion_ota/builds/$1.json
 else
-    echo "Generating JSON for OTA-VANILLA..."
+    echo "Generating JSON for OTA Vanilla Variant..."
     variant="A15_vanilla"
-    existingOTAjson=./orion/OTA-VANILLA/builds/$1.json
+    existingOTAjson=./orion/orion_ota-vanilla/builds/$1.json
 fi
 
 if [ -f $existingOTAjson ]; then
@@ -41,10 +41,10 @@ if [ -f $existingOTAjson ]; then
 	oem=`grep -n "\"oem\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
 	device=`grep -n "\"device\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
 	filename=$3
-	version=`echo "$3" | cut -d'-' -f5`
-	v_max=`echo "$version" | cut -d'.' -f1 | cut -d'v' -f2`
-	v_min=`echo "$version" | cut -d'.' -f2`
-	version=`echo $v_max.$v_min`
+  version=echo "$2" | cut -d'-' -f3
+	v_max=echo "$version" | cut -d'.' -f1 | cut -d'v' -f2
+	v_min=echo "$version" | cut -d'.' -f2
+	version=echo $v_max.$v_min
 	buildprop=$2/system/build.prop
 	linenr=`grep -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
 	timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
