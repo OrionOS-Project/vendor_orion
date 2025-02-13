674070cec9375a5a69d97f7d202827df01d1366c
Hirokixd <86230878+Wrdn28@users.noreply.github.com>
2025-02-13 12:06:57 +0700

    orion: drop AudioFX Packages

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -17,11 +17,6 @@ PRODUCT_PACKAGES += \
     Aperture
 endif
 
-ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
-PRODUCT_PACKAGES += \
-    AudioFX
-endif
-
 # Extra cmdline tools
 PRODUCT_PACKAGES += \
     unrar \
